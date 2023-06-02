815e364a37045baf879b40ea10becfc062dd032f
Egor Kruglov <yegorkruglov@gmail.com>
2023-06-02 13:46:38 +0300

    login2

---
+++ LOgin2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LOgin2ViewController.swift
+//  testApp
+//
+//  Created by Egor Kruglov on 02.06.2023.
+//
+
+import UIKit
+
+class LOgin2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
